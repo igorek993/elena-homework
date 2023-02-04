4a0f76cb031400ac95c00286e33d21cfca1a9551
Elena K <elenakhlebnikova94@gmail.com>
2023-02-04 17:05:43 +1100

    ошибка

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -12,11 +12,34 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = dict()
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) **2) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) **2) ** .5
+
+distances['Moscow'] = {}
+distances['Moscow'] ['London'] = moscow_london
+distances['Moscow'] ['Paris'] = moscow_paris
 
 print(distances)
+
+london_paris = ((paris[0] - london[0]) ** 2 + (paris[1] - london[1]) **2) ** .5
+
+distances['Moscow'] = {}
+distances['London']['Moscow'] = moscow_london
+distances['London']['Paris'] = london_paris
+
+print(distances)
+
+
+
+
+
+
+
 
 
 
