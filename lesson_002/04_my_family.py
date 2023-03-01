6d486b94e624ace487a09d427a9a66de10ffc31c
Elena K <elenakhlebnikova94@gmail.com>
2023-03-01 15:38:06 +1100

    задание выполненно. Вместо отца я высчитала рост мужа.

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -4,17 +4,22 @@
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
 
+my_family = ['igor', 'alice', 'elena']
 
 # список списков приблизителного роста членов вашей семьи
-my_family_height = [
-    # ['имя', рост],
-    [],
-]
 
+my_family_height = [['igor', 178], ['alice',60 ], ['elena',164]]
+igor_height= 178
+alice_height= 60
+elena_height= 164
+print(igor_height , 'cm is my husbands height')
+
+    # ['имя', рост],
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+
+print(igor_height+alice_height+elena_height,'cm is my familys height')
