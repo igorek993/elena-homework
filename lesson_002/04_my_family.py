cef8cf7e093f5e8d0ae645356cfdaae41af6076e
Elena K <elenakhlebnikova94@gmail.com>
2023-03-04 13:45:52 +1100

    ошибки исправлены

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -5,15 +5,13 @@
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
 
-my_family = ['igor', 'alice', 'elena']
+my_family = ['Igor', 'Alice', 'Elena']
 
 # список списков приблизителного роста членов вашей семьи
 
 my_family_height = [['igor', 178], ['alice',60 ], ['elena',164]]
-igor_height= 178
-alice_height= 60
-elena_height= 164
-print('my husbands height is', igor_height, 'cm')
+fathers_height =my_family_height[0][1]
+print('fathers height is -',fathers_height,'cm')
 
     # ['имя', рост],
 # Выведите на консоль рост отца в формате
@@ -22,4 +20,8 @@ print('my husbands height is', igor_height, 'cm')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
 
-print('my familys height is', igor_height+alice_height+elena_height ,'cm')
+igors_height= 178
+alices_height=60
+elenas_height=164
+print('total family height - ', igors_height+alices_height+elenas_height,'cm')
+
