f556c49f9bfef47311d6bcfc73c9d6a55e8b9c53
Elena K <elenakhlebnikova94@gmail.com>
2023-03-19 16:04:13 +1100

    all done

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -9,23 +9,21 @@ my_family = ['Igor', 'Alice', 'Elena']
 
 # список списков приблизителного роста членов вашей семьи
 
-my_family_height = [['igor', 178], ['alice',60 ], ['elena',164]]
+my_family_height = [['igor', 178], ['alice', 60], ['elena', 164]]
 
-
-    # ['имя', рост],
+# ['имя', рост],
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
 
-fathers_height =my_family_height[0][1]
-print('fathers height is -',fathers_height,'cm')
+fathers_height = my_family_height[0][1]
+print('fathers height is -', fathers_height, 'cm')
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
 
 
-igors_height= my_family_height[0][1]
-alices_height=my_family_height[1][1]
-elenas_height=my_family_height[2][1]
-fh=igors_height + alices_height + elenas_height
-print('total family height - ', fh,'cm')
-
+igors_height = my_family_height[0][1]
+alices_height = my_family_height[1][1]
+elenas_height = my_family_height[2][1]
+family_height = igors_height + alices_height + elenas_height
+print('total family height - ', family_height, 'cm')
